bcccebb58644444ab4c8bc008f1d2eeac90b9379
CloudWhiteTower <2268742456@qq.com>
2026-05-05 00:57:24 +0800

    Initial Commit

---
+++ Qizip/QizipApp.swift
@@ -0,0 +1,17 @@
+//
+//  QizipApp.swift
+//  Qizip
+//
+//  Created by CloudWhiteTower on 2026/5/5.
+//
+
+import SwiftUI
+
+@main
+struct QizipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
